63158964a6601c95381733a02072d1d030504580
Charles Ashby <charlesa.l@hotmail.com>
2017-05-20 18:52:12 -0400

    Add files via upload

---
+++ lib_model/char_lstm.py
@@ -0,0 +1,230 @@
+import tensorflow as tf
+from tensorflow.contrib import rnn
+
+import numpy as np
+import cPickle
+from lib.data_utils import *
+from lib.ops import *
+
+PATH = '/home/ashbylepoc/PycharmProjects/tensorflow/'
+TRAIN_SET = PATH + 'datasets/train_set.csv'
+TEST_SET = PATH + 'datasets/test_set.csv'
+VALID_SET = PATH + 'datasets/valid_set.csv'
+SAVE_PATH = PATH + 'checkpoints/lstm.ckpt'
+LOGGING_PATH = PATH + 'checkpoints/log.txt'
+
+class LSTM(object):
+    """ Character-Level LSTM Implementation """
+
+    def __init__(self):
+        # X is of shape ('b', 'sentence_length', 'max_word_length', 'alphabet_size')
+        self.hparams = self.get_hparams()
+        max_word_length = self.hparams['max_word_length']
+        self.X = tf.placeholder('float32', shape=[None, None, max_word_length, ALPHABET_SIZE], name='X')
+        self.Y = tf.placeholder('float32', shape=[None, 2], name='Y')
+
+    def build(self,
+              kernels=[1, 2, 3, 4, 5, 6, 7],
+              kernel_features=[25, 50, 75, 100, 125, 150, 175],
+              rnn_size=650,
+              dropout=0.5,
+              size=700,
+              train_samples=1600000 * 0.95,
+              valid_samples=1600000 * 0.05):
+
+        self.size = size
+        self.hparams = self.get_hparams()
+        self.max_word_length = self.hparams['max_word_length']
+        self.train_samples = train_samples
+        BATCH_SIZE = self.hparams['BATCH_SIZE']
+
+        def highway(input_, size, num_layers=1, bias=-2.0, f=tf.nn.relu, scope='Highway'):
+            """Highway Network (cf. http://arxiv.org/abs/1505.00387).
+            t = sigmoid(Wy + b)
+            z = t * g(Wy + b) + (1 - t) * y
+            where g is nonlinearity, t is transform gate, and (1 - t) is carry gate.
+            """
+
+            with tf.variable_scope(scope):
+                for idx in range(num_layers):
+                    g = f(linear(input_, size, scope='highway_lin_%d' % idx))
+
+                    t = tf.sigmoid(linear(input_, size, scope='highway_gate_%d' % idx) + bias)
+
+                    output = t * g + (1. - t) * input_
+                    input_ = output
+
+            return output
+
+        def tdnn(input_, kernels, kernel_features, scope='TDNN'):
+            ''' Time Delay Neural Network
+            :input:           input float tensor of shape [(batch_size*num_unroll_steps) x max_word_length x embed_size]
+            :kernels:         array of kernel sizes
+            :kernel_features: array of kernel feature sizes (parallel to kernels)
+            '''
+            assert len(kernels) == len(kernel_features), 'Kernel and Features must have the same size'
+
+            # input_ is a np.array of shape ('b', 'sentence_length', 'max_word_length', 'embed_size') we
+            # need to convert it to shape ('b * sentence_length', 1, 'max_word_length', 'embed_size') to
+            # use conv2D
+            input_ = tf.reshape(input_, [-1, self.max_word_length, ALPHABET_SIZE])
+            input_ = tf.expand_dims(input_, 1)
+
+            layers = []
+            with tf.variable_scope(scope):
+                for kernel_size, kernel_feature_size in zip(kernels, kernel_features):
+                    reduced_length = self.max_word_length - kernel_size + 1
+
+                    # [batch_size * sentence_length x max_word_length x embed_size x kernel_feature_size]
+                    conv = conv2d(input_, kernel_feature_size, 1, kernel_size, name="kernel_%d" % kernel_size)
+
+                    # [batch_size * sentence_length x 1 x 1 x kernel_feature_size]
+                    pool = tf.nn.max_pool(tf.tanh(conv), [1, 1, reduced_length, 1], [1, 1, 1, 1], 'VALID')
+
+                    layers.append(tf.squeeze(pool, [1, 2]))
+
+                if len(kernels) > 1:
+                    output = tf.concat(layers, 1)
+                else:
+                    output = layers[0]
+
+            return output
+
+        cnn = tdnn(self.X, kernels, kernel_features)
+
+        # tdnn() returns a tensor of shape [batch_size * sentence_length x kernel_features]
+        # highway() returns a tensor of shape [batch_size * sentence_length x size] to use
+        # tensorflow dynamic_rnn module we need to reshape it to [batch_size x sentence_length x size]
+        cnn = highway(cnn, self.size)
+        cnn = tf.reshape(cnn, [BATCH_SIZE, -1, self.size])
+
+        with tf.variable_scope('LSTM'):
+
+            def create_rnn_cell():
+                cell = rnn.BasicLSTMCell(rnn_size, state_is_tuple=True, forget_bias=0.0, reuse=False)
+
+                if dropout > 0.0:
+                    cell = tf.contrib.rnn.DropoutWrapper(cell, output_keep_prob=1. - dropout)
+
+                return cell
+
+            cell = create_rnn_cell()
+            initial_rnn_state = cell.zero_state(BATCH_SIZE, dtype='float32')
+
+            outputs, final_rnn_state = tf.nn.dynamic_rnn(cell, cnn,
+                                                         initial_state=initial_rnn_state,
+                                                         dtype=tf.float32)
+
+            # In this implementation, we only care about the last outputs of the RNN
+            # i.e. the output at the end of the sentence
+            outputs = tf.transpose(outputs, [1, 0, 2])
+            last = outputs[-1]
+
+        self.prediction = softmax(last, 2)
+
+    def train(self):
+        BATCH_SIZE = self.hparams['BATCH_SIZE']
+        EPOCHS = self.hparams['EPOCHS']
+        max_word_length = self.hparams['max_word_length']
+        learning_rate = self.hparams['learning_rate']
+
+        pred = self.prediction
+
+        cost = - tf.reduce_sum(self.Y * tf.log(tf.clip_by_value(pred, 1e-10, 1.0)))
+
+        predictions = tf.equal(tf.argmax(pred, 1), tf.argmax(self.Y, 1))
+
+        acc = tf.reduce_mean(tf.cast(predictions, 'float32'))
+
+        optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
+
+        n_batch = self.train_samples // BATCH_SIZE
+
+        # parameters for saving and early stopping
+        saver = tf.train.Saver()
+        patience = self.hparams['patience']
+
+        with tf.Session() as sess:
+            sess.run(tf.initialize_all_variables())
+            best_acc = 0.0
+
+            for epoch in range(EPOCHS):
+                loss = 0.0
+                batch = 1
+
+
+                with open(TRAIN_SET, 'r') as f:
+                    reader = TextReader(f, max_word_length)
+                    for minibatch in reader.iterate_minibatch(BATCH_SIZE, dataset=TRAIN_SET):
+                        batch_x, batch_y = minibatch
+
+                        _, c, a = sess.run([optimizer, cost, acc], feed_dict={self.X: batch_x, self.Y: batch_y})
+
+                        loss += c
+
+                        if batch % 100 == 0:
+                            # Compute Accuracy on the Training set and print some info
+                            print('Epoch: %5d/%5d -- batch: %5d/%5d -- Loss: %.4f -- Train Accuracy: %.4f' %
+                                  (epoch, EPOCHS, batch, n_batch, loss/batch, a))
+
+                            # Write loss and accuracy to some file
+                            log = open(LOGGING_PATH, 'a')
+                            log.write('%s, %6d, %.5f, %.5f' % ('train', epoch * batch, loss/batch, a))
+                            log.close()
+
+                        # --------------
+                        # EARLY STOPPING
+                        # --------------
+
+                        # Compute Accuracy on the Validation set, check if validation has improved, save model, etc
+                        if batch % 500 == 0:
+                            accuracy = []
+
+                            # Validation set is very large, so validation accuracy is done on testing set
+                            # instead, change TEST_SET to VALID_SET to compute accuracy on valid set
+                            with open(TEST_SET, 'r') as ff:
+                                valid_reader = TextReader(ff, max_word_length)
+                                for mb in valid_reader.iterate_minibatch(BATCH_SIZE, dataset=TEST_SET):
+                                    valid_x, valid_y = mb
+                                    a = sess.run([acc], feed_dict={self.X: valid_x, self.Y: valid_y})
+                                    accuracy.append(a)
+                                mean_acc = np.mean(accuracy)
+
+                                # if accuracy has improved, save model and boost patience
+                                if mean_acc > best_acc:
+                                    best_acc = mean_acc
+                                    save_path = saver.save(sess, SAVE_PATH)
+                                    patience = self.hparams['patience']
+                                    print('Model saved in file: %s' % save_path)
+
+                                # else reduce patience and break loop if necessary
+                                else:
+                                    patience -= 500
+                                    if patience <= 0:
+                                        break
+
+                                print('Epoch: %5d/%5d -- batch: %5d/%5d -- Valid Accuracy: %.4f' %
+                                     (epoch, EPOCHS, batch, n_batch, mean_acc))
+
+                                # Write validation accuracy to log file
+                                log = open(LOGGING_PATH, 'a')
+                                log.write('%s, %6d, %.5f' % ('valid', epoch * batch, mean_acc))
+                                log.close()
+
+                        batch += 1
+
+    def get_hparams(self):
+        ''' Get Hyperparameters '''
+
+        return {
+            'BATCH_SIZE':       64,
+            'EPOCHS':           500,
+            'max_word_length':  16,
+            'learning_rate':    0.0001,
+            'patience':         10000,
+        }
+
+if __name__ == '__main__':
+    network = LSTM()
+    network.build()
+    network.train()
