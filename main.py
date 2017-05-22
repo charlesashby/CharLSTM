27daded6666737bfff09867bc6b6449017dd2bac
Charles Ashby <charlesa.l@hotmail.com>
2017-05-22 15:46:44 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,18 +2,35 @@ import argparse
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Load a model')
-    parser.add_argument('model', action="store", type=str, dest='model')
+    parser.add_argument('model', action="store", type=str)
+    parser.add_argument('--train', action='store_true')
+    parser.add_argument('--sentences', nargs='*', default=None)
+
     args = parser.parse_args()
 
-    if args.model == 'LSTM':
-        from lib_models.char_lstm import *
+    print('Using model: %s' % args.model)
+    print('Training: %s' % args.train)
+    if args.sentences is not None:
+        for value in args.sentences:
+            print('processing sentence: %s' % value)
+
+    if args.model == 'lstm':
+        from lib_model.char_lstm import *
+
         network = LSTM()
         network.build()
-        network.train()
-        
-    elif args.model == 'bidirectional_LSTM':
-        from lib_models.bidirectional_lstm import *
+        if args.train == True:
+            network.train()
+        if args.sentences is not None:
+            network.predict_sentences(args.sentences)
+
+    elif args.model == 'bidirectional_lstm':
+        from lib_model.bidirectional_lstm import *
+
         network = LSTM()
         network.build()
-        network.train()
-        
+        if args.train == True:
+            network.train()
+        if args.sentences is not None:
+            network.predict_sentences(args.sentences)
+
